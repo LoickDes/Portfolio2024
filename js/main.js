6d8a0e99dd1ea9992a26c679b60606bfd2633541
Loïck <119866900+LoickDes@users.noreply.github.com>
2024-01-03 16:07:08 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -72,3 +72,6 @@ $(document).ready(function(){
       
 
   });
+
+
+  
---
+++ js/scrolltrigger.js
@@ -0,0 +1,20 @@
+
+const observer = new IntersectionObserver((entries)=>{
+
+    entries.forEach((entry)=>{
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        }
+
+    });    
+});
+
+const hiddenleftElements = document.querySelectorAll('.hidden-left');
+hiddenleftElements.forEach((el)=>observer.observe(el));
+
+const hiddenrightElements = document.querySelectorAll('.hidden-right');
+hiddenrightElements.forEach((el)=>observer.observe(el));
+
+const hiddenbottomElements = document.querySelectorAll('.hidden-bottom');
+hiddenbottomElements.forEach((el)=>observer.observe(el));
